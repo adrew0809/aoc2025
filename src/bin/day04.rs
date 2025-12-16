addcf8c99a72cdd1f72ee78e23461bfd73604702
A. Drew <aedrew@gmail.com>
2025-12-15 21:59:02 -0800

    solve day 4 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -2,11 +2,29 @@
 // A. Drew
 
 fn main() {
-    let accessible = std::fs::read_to_string("data/day04/input.txt").map(|text| {
+    use std::ops::ControlFlow;
+
+    let answer0 = std::fs::read_to_string("data/day04/input.txt").map(|text| {
         let rolls = parse_rolls(&text);
-        find_accessible(&rolls).len()
+        let (accessible, _) = partition_accessible(&rolls);
+        accessible.len()
     });
-    println!("{:?}", accessible);
+    println!("{:?}", answer0);
+
+    let answer1 = std::fs::read_to_string("data/day04/input.txt").map(|text| {
+        let rolls = parse_rolls(&text);
+        std::iter::repeat(()).try_fold((0, rolls), |(cnt, rolls), _| {
+            let (removed, remaining) = partition_accessible(&rolls);
+            let n = removed.len();
+            if n == 0 {
+                ControlFlow::Break(cnt)
+            } else {
+                ControlFlow::Continue((cnt + n, remaining))
+            }
+        })
+    });
+
+    println!("{:?}", answer1);
 }
 
 fn parse_rolls(text: &str) -> Vec<[isize; 2]> {
@@ -21,18 +39,15 @@ fn parse_rolls(text: &str) -> Vec<[isize; 2]> {
         .collect()
 }
 
-fn find_accessible(rolls: &[[isize; 2]]) -> Vec<[isize; 2]> {
-    rolls
-        .iter()
-        .filter(|[i, j]| {
-            let neighbors = (i - 1..=i + 1)
-                .flat_map(|ii| (j - 1..=j + 1).map(move |jj| [ii, jj]))
-                .filter(|x| rolls.binary_search(x).is_ok())
-                .count();
-            neighbors < (4 + 1)
-        })
-        .copied()
-        .collect()
+// return the accessible rolls that were removed and the remaining rolls
+fn partition_accessible(rolls: &[[isize; 2]]) -> (Vec<[isize; 2]>, Vec<[isize; 2]>) {
+    rolls.iter().partition(|[i, j]| {
+        let neighbors = (i - 1..=i + 1)
+            .flat_map(|ii| (j - 1..=j + 1).map(move |jj| [ii, jj]))
+            .filter(|x| rolls.binary_search(x).is_ok())
+            .count();
+        neighbors < (4 + 1)
+    })
 }
 
 #[cfg(test)]
@@ -133,30 +148,8 @@ mod tests {
     }
 
     #[test]
-    fn test_sample_0() {
-        let [i, j] = [1, 2];
-        let neighbors = (i - 1..=i + 1)
-            .flat_map(|ii| (j - 1..=j + 1).map(move |jj| [ii, jj]))
-            .collect::<Vec<_>>();
-        assert_eq!(
-            neighbors,
-            [
-                [0, 1],
-                [0, 2],
-                [0, 3],
-                [1, 1],
-                [1, 2],
-                [1, 3],
-                [2, 1],
-                [2, 2],
-                [2, 3]
-            ]
-        );
-    }
-
-    #[test]
     fn test_sample() {
-        let accessible = find_accessible(&ROLLS);
+        let (accessible, _) = partition_accessible(&ROLLS);
         assert_eq!(
             accessible,
             [
@@ -173,6 +166,63 @@ mod tests {
                 [9, 0],
                 [9, 2],
                 [9, 8]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_sample_1() {
+        let (accessible, left) = partition_accessible(&ROLLS);
+        assert_eq!(accessible.len(), 13);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 12);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 7);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 5);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 2);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 1);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 1);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 1);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 1);
+        let (accessible, left) = partition_accessible(&left);
+        assert_eq!(accessible.len(), 0);
+        assert_eq!(
+            left,
+            [
+                [3, 4],
+                [3, 5],
+                [4, 3],
+                [4, 4],
+                [4, 5],
+                [4, 6],
+                [5, 3],
+                [5, 4],
+                [5, 5],
+                [5, 6],
+                [5, 7],
+                [6, 3],
+                [6, 5],
+                [6, 7],
+                [6, 8],
+                [7, 3],
+                [7, 4],
+                [7, 6],
+                [7, 7],
+                [7, 8],
+                [8, 3],
+                [8, 4],
+                [8, 5],
+                [8, 6],
+                [8, 7],
+                [9, 4],
+                [9, 5],
+                [9, 6],
             ]
         );
     }
