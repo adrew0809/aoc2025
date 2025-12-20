87e02ff9985b8bbcfdc5aac19089b908ff7c32a2
A. Drew <aedrew@gmail.com>
2025-12-20 09:01:00 -0800

    solve day 5 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -8,17 +8,24 @@ use nom::{
     sequence::separated_pair,
     Parser,
 };
+use std::cmp::{max, min};
+use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
 fn main() {
-    let answer0 = std::fs::read_to_string("data/day05/input.txt")
+    let answers = std::fs::read_to_string("data/day05/input.txt")
         .and_then(|text| {
             parse_database(&text)
                 .map_err(|e| std::io::Error::other(e.to_owned()))
                 .map(|(_, r)| r)
         })
-        .map(|(fresh, ingredients)| fresh_ingredients(&fresh, &ingredients).len());
-    println!("{:?}", answer0);
+        .map(|(fresh, ingredients)| {
+            (
+                fresh_ingredients(&fresh, &ingredients).len(),
+                merge_ranges(&fresh).iter().map(count_elements).sum::<u64>(),
+            )
+        });
+    println!("{:?}", answers);
 }
 
 fn parse_frash_range(input: &str) -> nom::IResult<&str, RangeInclusive<u64>> {
@@ -53,6 +60,34 @@ fn fresh_ingredients(fresh: &[RangeInclusive<u64>], ingredients: &[u64]) -> Vec<
         .filter(|x| is_fresh(fresh, **x))
         .copied()
         .collect()
+}
+
+fn overlaps(r0: &RangeInclusive<u64>, r1: &RangeInclusive<u64>) -> bool {
+    r0.contains(r1.start()) || r0.contains(r1.end())
+}
+
+fn merge_overlapping(ranges: &[RangeInclusive<u64>]) -> Option<RangeInclusive<u64>> {
+    ranges.iter().cloned().reduce(|acc, r| {
+        let start = min(*acc.start(), *r.start());
+        let end = max(*acc.end(), *r.end());
+        start..=end
+    })
+}
+
+fn count_elements(range: &RangeInclusive<u64>) -> u64 {
+    range.end() - range.start() + 1
+}
+
+fn merge_ranges(fresh: &[RangeInclusive<u64>]) -> HashSet<RangeInclusive<u64>> {
+    fresh
+        .iter()
+        .fold(HashSet::from_iter(fresh.iter().cloned()), |mut acc, r| {
+            let overlapping: Vec<_> = acc.extract_if(|x| overlaps(r, x)).collect();
+            if let Some(merged) = merge_overlapping(&overlapping) {
+                acc.insert(merged);
+            }
+            acc
+        })
 }
 
 #[cfg(test)]
@@ -92,5 +127,12 @@ mod tests {
         let (_, (ranges, ingredients)) = parse_database(TEXT).unwrap();
         assert_eq!(ranges, FRESH);
         assert_eq!(ingredients, IDS);
+    }
+
+    #[test]
+    fn test_all_fresh() {
+        let ans = merge_ranges(&FRESH);
+        assert_eq!(ans, HashSet::from([3..=5, 10..=20]));
+        assert_eq!(ans.iter().map(count_elements).sum::<u64>(), 14);
     }
 }
