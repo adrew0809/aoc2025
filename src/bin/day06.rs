f9ad57b8c85af5ccfbb4c9fe5d7dc6f49516b887
A. Drew <aedrew@gmail.com>
2025-12-23 13:13:35 -0800

    Solve day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,71 +1,78 @@
 // Advent of Code 2025 Day 6
 // A. Drew
 
-use nom::{
-    character::complete::{char, digit1, newline, space0, space1},
-    combinator::value,
-    error::ParseError,
-    multi::{many1, separated_list1},
-    sequence::{delimited, terminated},
-    IResult, Parser,
-};
 use std::ops::{Add, Mul};
 
 type Op = fn(u64, u64) -> u64;
 
 fn main() {
-    let answers: Result<u64, _> = std::fs::read_to_string("data/day06/input.txt")
-        .and_then(|text| {
-            parse_problems(&text)
-                .map_err(|e| std::io::Error::other(e.to_owned()))
-                .map(|(_, r)| r)
-        })
-        .map(|(matrix, ops)| apply(&ops, &matrix).iter().sum());
-    println!("{:?}", answers);
+    let text = std::fs::read_to_string("data/day06/input.txt").unwrap();
+    let lines: Vec<_> = text.lines().collect();
+    let (operator_line, operand_lines) = lines.split_last().unwrap();
+    let operators = parse_operators(operator_line);
+    let ans0: u64 = solve_rows(&operators, operand_lines).iter().sum();
+    let ans1: u64 = solve_cols(&operators, operand_lines).iter().sum();
+    println!("answer 0: {}", ans0);
+    println!("answer 1: {}", ans1);
 }
 
-fn apply(ops: &[Op], matrix: &[u64]) -> Vec<u64> {
-    ops.iter()
-        .enumerate()
-        .map(|(i, op)| {
-            matrix
+fn solve_rows(operators: &[(usize, usize, Op)], operand_lines: &[&str]) -> Vec<u64> {
+    operators
+        .iter()
+        .map(|(i, w, op)| {
+            operand_lines
                 .iter()
-                .skip(i)
-                .step_by(ops.len())
-                .copied()
+                .map(move |line| line.chars().skip(*i).take(*w).collect::<String>())
+                .map(|s| s.trim().parse().expect("all digit input"))
                 .reduce(op)
-                .expect("at least one row of input")
+                .expect("at least one line")
         })
         .collect()
 }
 
-fn parse_problems(text: &str) -> IResult<&str, (Vec<u64>, Vec<Op>)> {
-    many1(row_of(number_parser))
-        .map(|v| v.into_iter().flatten().collect())
-        .and(row_of(op_parser))
-        .parse(text)
+fn solve_cols(operators: &[(usize, usize, Op)], operand_lines: &[&str]) -> Vec<u64> {
+    operators
+        .iter()
+        .map(|(i, w, op)| {
+            (0..*w)
+                .map(|j| {
+                    let digits: String = operand_lines
+                        .iter()
+                        .map(|line| line.chars().nth(i + j).expect("digits all in line"))
+                        .collect();
+                    digits.trim().parse().expect("all digit input")
+                })
+                .reduce(op)
+                .expect("at least one line")
+        })
+        .collect()
 }
 
-fn op_parser(input: &str) -> IResult<&str, Op> {
-    value(u64::add as Op, char('+'))
-        .or(value(u64::mul as Op, char('*')))
-        .parse(input)
-}
-
-fn number_parser(input: &str) -> IResult<&str, u64> {
-    digit1.map_res(str::parse).parse(input)
-}
-
-fn row_of<'a, O, E: ParseError<&'a str>, F>(
-    inner: F,
-) -> impl Parser<&'a str, Output = Vec<O>, Error = E>
-where
-    F: Parser<&'a str, Output = O, Error = E>,
-{
-    terminated(
-        delimited(space0, separated_list1(space1, inner), space0),
-        newline,
-    )
+fn parse_operators(input: &str) -> Vec<(usize, usize, Op)> {
+    input
+        .char_indices()
+        .fold(Vec::<(usize, usize, Op)>::new(), |mut acc, (i, c)| {
+            match c {
+                '*' => {
+                    if let Some((_, w, _)) = acc.last_mut() {
+                        *w -= 1;
+                    }
+                    acc.push((i, 1, u64::mul));
+                }
+                '+' => {
+                    if let Some((_, w, _)) = acc.last_mut() {
+                        *w -= 1;
+                    }
+                    acc.push((i, 1, u64::add));
+                }
+                _ => {
+                    if let Some((_, w, _)) = acc.last_mut() {
+                        *w += 1;
+                    }
+                }
+            }
+            acc
+        })
 }
 
 #[cfg(test)]
@@ -79,20 +86,30 @@ mod tests {
 *   +   *   +  
 ";
 
-    const MATRIX: [u64; 3 * 4] = [123, 328, 51, 64, 45, 64, 387, 23, 6, 98, 215, 314];
-
-    const OPS: [Op; 4] = [u64::mul, u64::add, u64::mul, u64::add];
+    #[test]
+    fn test_paser_operators() {
+        let line = "*   +    *   +  ";
+        let ops = parse_operators(line);
+        assert_eq!(ops.len(), 4);
+        let os: Vec<_> = ops.iter().map(|(i, w, _)| (*i, *w)).collect();
+        assert_eq!(os, [(0, 3), (4, 4), (9, 3), (13, 3)]);
+    }
 
     #[test]
-    fn test_apply() {
-        let ans = apply(&OPS, &MATRIX);
+    fn test_solve_rows() {
+        let lines: Vec<_> = TEXT.lines().collect();
+        let (operator_line, operand_lines) = lines.split_last().unwrap();
+        let operators = parse_operators(operator_line);
+        let ans = solve_rows(&operators, operand_lines);
         assert_eq!(ans, [33210, 490, 4243455, 401]);
     }
 
     #[test]
-    fn test_parse() {
-        let (_, (ms, os)) = parse_problems(TEXT).unwrap();
-        assert_eq!(ms, MATRIX);
-        assert_eq!(os, OPS);
+    fn test_solve_cols() {
+        let lines: Vec<_> = TEXT.lines().collect();
+        let (operator_line, operand_lines) = lines.split_last().unwrap();
+        let operators = parse_operators(operator_line);
+        let ans = solve_cols(&operators, operand_lines);
+        assert_eq!(ans, [8544, 625, 3253600, 1058]);
     }
 }
